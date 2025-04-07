2338d9c5f95f50a2eb1c555b1d34d9e28a146654
maciejg-git <m.gralek@gmail.com>
2025-04-07 19:05:17 +0200

    update

--- init.lua
+++ init.lua
@@ -467,9 +467,6 @@ require("lazy").setup({
       end,
     },
     {
-      'mbbill/undotree',
-    },
-    {
       "folke/which-key.nvim",
     },
     {
@@ -642,9 +639,6 @@ require("lazy").setup({
       priority = 1000,
       lazy = false,
       opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
         picker = {
           layout = {
             preset = "telescope",
