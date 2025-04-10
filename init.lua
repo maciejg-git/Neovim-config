5373dea785241220dd1cc595e99f5b9ca3e7e4b7
maciejg-git <m.gralek@gmail.com>
2025-04-10 22:22:09 +0200

    update

--- init.lua
+++ init.lua
@@ -495,6 +495,8 @@ require("lazy").setup({
     },
     {
       "folke/flash.nvim",
+      event = "VeryLazy",
+      opts = {},
       keys = {
         { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
         { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
