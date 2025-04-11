89376ed22a22d2e7c2a60d11799a407a424405f8
maciejg-git <m.gralek@gmail.com>
2025-04-11 15:40:52 +0200

    update

--- init.lua
+++ init.lua
@@ -327,7 +327,6 @@ require("lazy").setup({
             init_options = {
               html = {
                 options = {
-                  -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
                   ["bem.enabled"] = true,
                 },
               },
